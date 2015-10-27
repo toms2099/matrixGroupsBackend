4d153bfa443d02dfe9a81946a041d27dcc736f4b
unknown <TSTEAD@TSTEAD03.corp.amdocs.com>
2015-10-27 17:35:21 +0000

    stuff

--- src/main/java/com/openmarket/Controller.java
+++ src/main/java/com/openmarket/Controller.java
@@ -3,7 +3,11 @@ package com.openmarket;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.ws.rs.PathParam;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -41,12 +45,23 @@ public class Controller {
 		return peeps;
 	}
 	
+	@Autowired
+	GroupMapper gm;
+	
 	@RequestMapping(value="perform", method=RequestMethod.POST, produces="application/json")
 	Result perform(@RequestBody Body body) {
+		
+		gm.getUsers("test");
 		
 		List<String> peeps = getList(body.keyword);
 		
 	    matrix.createRoom("candyTestRoom2", peeps);
 	    return new Result();
+	}
+	
+	@RequestMapping(value="contactGroups/{id}", method=RequestMethod.GET, produces="application/json")
+	List<String> get(@PathVariable String id) {
+		
+		return gm.getUsers(id);
 	}
 }
---
+++ src/main/java/com/openmarket/GroupMapper.java
@@ -0,0 +1,66 @@
+package com.openmarket;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.io.CharStreams;
+
+@Component
+public class GroupMapper {
+	private ObjectMapper mapper = new ObjectMapper();
+	
+	@Autowired
+	private ApplicationContext appContext;
+	
+	
+	public static class ContactGroups {
+		public List<String> contacts;
+	}
+	
+	public List<String> getUsers(String keyword) {
+	    Resource res = appContext.getResource("classpath:contactGroups.json");	    
+	    String json = "";
+	    
+	    try {
+			json = CharStreams.toString(new InputStreamReader(res.getInputStream()));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}    
+	    
+	    TypeReference<HashMap<String,ContactGroups>> typeRef = new TypeReference<HashMap<String,ContactGroups>>() {};
+	    
+	    Map<String, ContactGroups> cg = null;
+	    
+	    try {
+			cg = mapper.readValue(res.getInputStream(), typeRef);
+		} catch (JsonParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    
+	    Object obj = cg.get(keyword);
+	    
+	    List<String> contacts = cg.get(keyword).contacts;
+	    
+	    return contacts;
+	}
+}
